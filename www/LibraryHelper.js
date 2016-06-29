92e4223d651110ec6f37e9c8f3aa588078016433
FlyingDonkey <jsosa@flyingdonkey.es>
2016-06-29 13:53:07 +0200

    Add files via upload

--- www/LibraryHelper.js
+++ www/LibraryHelper.js
@@ -2,14 +2,14 @@
 // https://github.com/coryjthompson/LibraryHelper-cordova
 var exec = require('cordova/exec');
 
-module.exports = {	
+module.exports = {
 	getVideoInfo: function(onSuccess, onError, path) {
 		exec(onSuccess, onError, 'LibraryHelper', 'getVideoInfo', [path]);
 	},
 	saveImageToLibrary: function (onSuccess, onError, path, albumName) {
 		exec(onSuccess, onError, 'LibraryHelper', 'saveImageToLibrary', [path, albumName]);
 	},
-	saveVideoToLibrary: function (onSuccess, onError, path) {
-		exec(onSuccess, onError, 'LibraryHelper', 'saveVideoToLibrary', [path]);
-	}
+  saveVideoToLibrary: function (onSuccess, onError, path, videoName) {
+    exec(onSuccess, onError, 'LibraryHelper', 'saveVideoToLibrary', [path, videoName]);
+  }
 };
